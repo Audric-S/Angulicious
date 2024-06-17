2cee7645826f4795e5f9dc9c6ede7fd8df633779
Audric-S <sabatieraudric@gmail.com>
2024-06-17 08:10:18 +0200

    :lipstick: add: starting form componenent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RecipesListComponent } from './components/receips-list/recipes-list.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { RecipeFormComponent } from './components/recipe-form/recipe-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, 
     RecipesListComponent,
-    NavbarComponent
+    NavbarComponent,
+    RecipeFormComponent
   ],
 
   templateUrl: './app.component.html',
--- src/app/components/recipe-form/recipe-form.component.ts
+++ src/app/components/recipe-form/recipe-form.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @Component({
   selector: 'app-recipe-form',
   standalone: true,
-  imports: [],
+  imports: [ ReactiveFormsModule, MatFormFieldModule],
   templateUrl: './recipe-form.component.html',
   styleUrl: './recipe-form.component.scss'
 })
 export class RecipeFormComponent {
+  recipeForm: FormGroup;
 
+  constructor(private _form_builder: FormBuilder) {
+    this.recipeForm = this._form_builder.group({
+      name: ['', Validators.required],
+      description: [''],
+      image: ['']
+    });
+  }
+
+  addRecipe() {
+    console.log(this.recipeForm.value);
+    this.recipeForm.reset();
+  }
 }
+
+
+
