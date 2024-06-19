79da0f1b2dcf0bb2fe7c4a21f3883e49ca9cd368
HugoooR <hugo.rochelle@gmail.com>
2024-06-19 19:00:06 +0200

    :hammer: finish ingredient input & send event to delete

--- src/app/components/ingredients-input/ingredients-input.component.ts
+++ src/app/components/ingredients-input/ingredients-input.component.ts
@@ -1,9 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { IngredientsService } from '../../services/ingredients.service';
 import { Ingredient } from '../../models/ingredient.model';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-ingredients-input',
@@ -11,7 +14,11 @@ import { Ingredient } from '../../models/ingredient.model';
   imports: [
     MatFormFieldModule,
     MatSelectModule,
-    CommonModule
+    CommonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatIconModule
   ],
   templateUrl: './ingredients-input.component.html',
   styleUrl: './ingredients-input.component.scss',
@@ -22,6 +29,8 @@ import { Ingredient } from '../../models/ingredient.model';
 
 export class IngredientsInputComponent {
 
+  @Output() ingredientDeleted = new EventEmitter<void>();
+
   constructor(
     protected ingredientService: IngredientsService,
   ){}
@@ -29,11 +38,14 @@ export class IngredientsInputComponent {
   options: Ingredient[] = [];
   selected = '';
   disableRemove: boolean = false;
+  quantity: number = 0;
 
 
   ngOnInit(){
     this.options = this.ingredientService.getAll();
   }
   
-  
+  deleteIngredient():void{
+    this.ingredientDeleted.emit();
+  }
 }
