d4f7f0be0fad19937898e7fdb3beb4dcbd5536df
Audric-S <sabatieraudric@gmail.com>
2024-06-30 00:20:57 +0200

    :construction: add: display recipe in cart in list page (TODO: move in header with dropdown menu

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,19 +4,23 @@ import { RecipeFormComponent } from '../recipe-form/recipe-form.component';
 import { CommonModule } from '@angular/common';
 import { Subject } from 'rxjs';
 import { Recipe } from '../../models/recipe.model';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RecipesListComponent,
     RecipeFormComponent,
-    CommonModule
+    CommonModule,
+    MatIcon
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   showForm: boolean = false;
+  currentRecipeOrder: Recipe[] = []
+
   eventsSubject: Subject<void> = new Subject<void>();
 
   emitEventToChild() {
@@ -28,11 +32,15 @@ export class HomeComponent {
   }
   
   addToCart(recipe: Recipe){
-    console.log('commande ajouté : ' + recipe.name)
+    this.currentRecipeOrder.push(recipe)
   }
 
   addRecipe(): void {
     this.toggleForm();
     this.emitEventToChild();
+  }
+
+  get itemCount(): number {
+    return this.currentRecipeOrder.length;
   }
 }
