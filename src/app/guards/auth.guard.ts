cac2be585b3ef76020f2dd7e84f25f215d7a224b
HugoooR <hugo.rochelle@gmail.com>
2024-06-30 19:23:45 +0200

    :bug: fix bug on guards, works as exepected

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,17 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { Injectable, inject } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { CookieService } from 'ngx-cookie-service';
 
 
 export const AuthGuard: CanActivateFn = (route, state) => {
-  const authService = inject(AuthService)
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
   if (authService.isAdmin()) {
     return true; 
   } else {
+    router.navigateByUrl("/home");
     return false;
   }
 };
